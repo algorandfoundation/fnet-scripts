db8a27dab20981f2153d40043396cc8570549128
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-08-01 15:28:27 +0300

    update-binary: download algod and goal

--- utils/update-binary.sh
+++ utils/update-binary.sh
@@ -14,45 +14,49 @@ sudo echo "" > /dev/null
 mkdir -p tmp
 
 SHASUMS_URL="https://fnet.algorand.green/static/SHA256SUMS"
-ALGOD_URL="https://fnet.algorand.green/static/algod"
+DOWNLOAD_URL_PREFIX="https://fnet.algorand.green/static"
 
-TMPFILE_SHA=$(realpath $(mktemp -p tmp -t SHA256SUMS-XXXXX))
-TMPFILE_ALGOD=$(realpath $(mktemp -p tmp -t algod-XXXXX))
-trap 'rm "$TMPFILE_SHA" "$TMPFILE_ALGOD"' EXIT
+TMPDIR=$(realpath $(mktemp -p tmp -d -t update-binary-XXXXXX))
+trap 'rm -rf "$TMPDIR"' EXIT
 
 echo "$LOGPFX downloading SHA256SUMS"
 
+TMPFILE_SHA="$TMPDIR/SHA256SUMS"
 curl -sf "$SHASUMS_URL" -o "$TMPFILE_SHA"
 
-LATEST_ALGOD_SHA=$(grep algod "$TMPFILE_SHA" | cut -d\  -f1)
+for file in algod goal; do
+    LATEST_SHA=$(grep $file "$TMPFILE_SHA" | cut -d\  -f1)
+    echo "$LOGPFX Latest $file: $LATEST_SHA"
 
-echo "$LOGPFX Latest algod $LATEST_ALGOD_SHA"
+    BINARY_PATH=$(which $file)
 
-ALGOD_PATH="$(which algod)"
+    CURRENT_SHA=$(sha256sum "$BINARY_PATH" | cut -d\  -f1)
+    echo "$LOGPFX Installed $file: $CURRENT_SHA"
 
-CURRENT_ALGOD_SHA=$(sha256sum "$ALGOD_PATH" | cut -d\  -f1)
-
-echo "$LOGPFX Installed algod $CURRENT_ALGOD_SHA"
-
-if [[ "$CURRENT_ALGOD_SHA" = "$LATEST_ALGOD_SHA" ]]; then
-    echo "$LOGPFX No update"
-else
-    echo "$LOGPFX algod has changed, updating"
-    echo "$LOGPFX Downloading algod"
-    curl -sf "$ALGOD_URL" -o "$TMPFILE_ALGOD"
-    DOWNLOADED_ALGOD_SHA=$(sha256sum "$TMPFILE_ALGOD" | cut -d\  -f1)
-    if [[ "$DOWNLOADED_ALGOD_SHA" = "$LATEST_ALGOD_SHA" ]]; then
-        echo "SHA256 hash matches. Installing"
-        sudo cp "$TMPFILE_ALGOD" "$ALGOD_PATH"
-        sudo chown "$USER:$USERGRP" "$ALGOD_PATH"
-        sudo chmod 755 "$ALGOD_PATH"
+    if [[ "$CURRENT_SHA" = "$LATEST_SHA" ]]; then
+        echo "$LOGPFX $file: no update"
     else
-        echo -e "$LOGPFX FATAL ERROR: Incorrect algod hash.\nExpected: $LATEST_ALGOD_SHA\nFound: $DOWNLOADED_ALGOD_SHA"
-        echo "Files left in tmp"
-        trap - EXIT
-        echo "$LOGPFX ERROR"
-        exit 1
+        TMPFILE_BINARY="$TMPDIR/$file"
+        echo "$LOGPFX $file: update available"
+
+        echo "$LOGPFX Downloading $file"
+        curl -sf "$DOWNLOAD_URL_PREFIX/$file" -o "$TMPFILE_BINARY"
+
+        DOWNLOADED_SHA=$(sha256sum "$TMPFILE_BINARY" | cut -d\  -f1)
+        if [[ "$DOWNLOADED_SHA" = "$LATEST_SHA" ]]; then
+            echo "$LOGPFX SHA256 hash verified."
+            echo "$LOGPFX Installing to $BINARY_PATH"
+            sudo cp "$TMPFILE_BINARY" "$BINARY_PATH"
+            sudo chown "$USER:$USERGRP" "$BINARY_PATH"
+            sudo chmod 755 "$BINARY_PATH"
+        else
+            echo -e "$LOGPFX ERROR: Incorrect $file hash.\nExpected: $LATEST_SHA\nFound: $DOWNLOADED_SHA"
+            echo "$LOGPFX Files left in $TMPDIR"
+            trap - EXIT
+            echo "$LOGPFX ERROR"
+            exit 1
+        fi
     fi
-fi
+done
 
 echo "$LOGPFX OK"
