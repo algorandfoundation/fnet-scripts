95ec3a382d4cb811a9ba50b2544c07ac909a80da
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-08-07 14:30:58 +0300

    catchup comapre GH and quit if mismatch

--- utils/catchup.sh
+++ utils/catchup.sh
@@ -15,6 +15,18 @@ if ! ./utils/is_node_syncing.sh; then
     exit 0
 fi
 
+# Check genesis hash
+LOCAL_GH=$($GOAL_CMD node status | grep "Genesis hash: " | cut -d\  -f3 | tr -d '\r')
+REMOTE_GH=$(curl -s 'https://fnet-api.4160.nodely.io/v2/transactions/params' | jq -r '.["genesis-hash"]')
+
+if [ "$LOCAL_GH" != "$REMOTE_GH" ]; then
+    echo "$LOGPFX Genesis hashes do not match"
+    echo "$LOGPFX Local $LOCAL_GH"
+    echo "$LOGPFX Remote $REMOTE_GH"
+    echo "$LOGPFX Aborting"
+    exit 1
+fi
+
 LAST_CP=$(curl -s https://fnet-api.4160.nodely.io/v2/status | jq -r '.["last-catchpoint"] // ""')
 
 if [[ "$LAST_CP" != "" ]]; then
